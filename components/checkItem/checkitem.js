2c2ece2acf923e829e4de1e79550bdf6c224ec4f
qianyf <635142555@qq.com>
2021-01-13 11:41:37 +0800

    update component css

--- components/checkItem/checkitem.js
+++ components/checkItem/checkitem.js
@@ -13,18 +13,7 @@ Component({
    * Component initial data
    */
   data: {
-    selectArray: [{
-      "value": "passed",
-      "showModal": false
-    }, {
-      "value": "Failed",
-      "showModal": false
-    },
-    {
-      "value": "手工输入",
-      "showModal": true
-    }
-  ]
+    
   },
 
   /**
