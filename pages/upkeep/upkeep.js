983776d004494c7dc9d1a3175998fcd273cab2d4
Han.Zhang <han.zhang_1@philips.com>
2021-01-21 16:18:51 +0800

    new-page

--- pages/upkeep/upkeep.js
+++ pages/upkeep/upkeep.js
@@ -7,6 +7,8 @@ Page({
   data: {
     choose: true,
     dateValue: '',
+    ticketBody: {},
+    ticketId: null,
   },
 
   /**
@@ -19,90 +21,137 @@ Page({
     var month = date.getMonth() + 1;
     var strDate = date.getDate();
     if (month >= 1 && month <= 9) {
-        month = "0" + month;
+      month = "0" + month;
     }
     if (strDate >= 0 && strDate <= 9) {
-        strDate = "0" + strDate;
+      strDate = "0" + strDate;
     }
     var currentdate = year + seperator1 + month + seperator1 + strDate;
 
-    this.setData({
-      dateValue:currentdate,
+    var ticketId = "01BD5357-2E0C-4A3D-A253-2C48500316A7"
+
+    var this_ = this
+
+    wx.request({
+      method: "GET",
+      url: 'http://localhost:8080/ticket/info?ticketId=' + ticketId,
+
+      success: function (res) {
+        this_.setData({
+          ticketBody: res.data
+        })
+      
+
+      }
     })
-  },
+    this.setData({
+      dateValue: currentdate,
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    })
   },
 
 
   chooseYes: function () {
+    var ticketBody = this.data.ticketBody
+    ticketBody.hololens = !ticketBody.hololens
     this.setData({
-      choose: !this.data.choose
+      ticketBody: ticketBody
     })
   },
 
   chooseNo: function () {
+    var ticketBody = this.data.ticketBody
+    ticketBody.hololens = !ticketBody.hololens
     this.setData({
-      choose: !this.data.choose
+      ticketBody: ticketBody
     })
   },
   datePickerBindchange: function (e) {
+    var ticketBody = this.data.ticketBody
+    ticketBody.deliveryDate = e.detail.value
     this.setData({
-      dateValue: e.detail.value,
+      ticketBody: ticketBody
     })
+    console.log(this.data.ticketBody)
   },
 
-  backSignPhoto: function(){
+  backPage: function () {
     wx.navigateTo({
       url: '/pages/signPhoto/signPhoto',
     })
-  }
+  },
+
+  nextPage: function () {
+    wx.request({
+      method: "POST",
+      url: 'http://localhost:8080/ticket/updateInfo',
+      data:this.data.ticketBody,
+      header: {'Content-Type': 'application/json'},
+
+      success: function (res) {
+        console.log(res)
+      }
+    })
+
+  },
+
+
+  updateHospitalName: function (e) {
+    var ticketBody = this.data.ticketBody
+    ticketBody.hospitalName = e.detail.value
+    this.setData({
+      ticketBody: ticketBody
+    })
+  },
+
+  updateProModal: function (e) {
+    var ticketBody = this.data.ticketBody
+    ticketBody.proModal = e.detail.value
+    this.setData({
+      ticketBody: ticketBody
+    })
+  },
+
+  updateProSerialNumber: function (e) {
+    var ticketBody = this.data.ticketBody
+    ticketBody.proSerialNumber = e.detail.value
+    this.setData({
+      ticketBody: ticketBody
+    })
+  },
+
+  updateEquipNumber: function (e) {
+    var ticketBody = this.data.ticketBody
+    ticketBody.equipNumber = e.detail.value
+    this.setData({
+      ticketBody: ticketBody
+    })
+  },
+
+  updateUpkeepInfo: function (e) {
+    var ticketBody = this.data.ticketBody
+    ticketBody.upkeepInfo = e.detail.value
+    this.setData({
+      ticketBody: ticketBody
+    })
+  },
+
+  updateSoftwareVersion: function (e) {
+    var ticketBody = this.data.ticketBody
+    ticketBody.softwareVersion = e.detail.value
+    this.setData({
+      ticketBody: ticketBody
+    })
+  },
+
+  updateEngineerName: function (e) {
+    var ticketBody = this.data.ticketBody
+    ticketBody.engineerName = e.detail.value
+    this.setData({
+      ticketBody: ticketBody
+    })
+  },
+
+
 
 })
